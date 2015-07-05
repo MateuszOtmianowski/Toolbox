24080684bdd02409698fbe72bc2f70426fa10266
MateuszOtmianowski <mateusz.otmianowski@gmail.com>
2015-07-05 10:49:58 +0200

    Create R_functions.R

---
+++ R_functions.R
@@ -0,0 +1,10 @@
+# Toolbox
+
+#splitting data into test and train sample with defined splitting ratio
+sample_splitting=function(seed,data_frame,dep_variable,split_ratio){
+        library(caTools)
+        set.seed(seed)
+        split=sample.split(dep_variable, SplitRatio=split_ratio)
+        train<<-subset(data_frame,split==TRUE)
+        test<<-subset(data_frame,split==TRUE)
+}
